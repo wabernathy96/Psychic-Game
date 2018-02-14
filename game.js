46a84529566c99830240bc41628ec8b7f42f1bdb
wabernathy96 <wabernathy96@gmail.com>
2018-02-14 00:45:12 -0600

    Restarted logic for js

--- game.js
+++ game.js
@@ -1,44 +1,33 @@
 //Universal variables for the game
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var wrongLetter = [];
-var userInput = [];
-var wins = 0;
-var losses = 0;
-var guessLeft = 10;
-// var randLetter;
+var losses = [];
+var gsLeft = 10;
 
-//Start the game by choosing a random letter 
+ 
 // alert ("Try to guess what letter I'm thinking of!");
-
 
 var randLetter = letters[Math.floor(Math.random() * letters.length)];
     console.log(randLetter);
 
-
-
-// document.getElementById("win").textContent = wins;
-
-//Player guesses letter
-document.getElementById("guess").textContent = userInput; 
-
-    document.onkeyup = function(event){
-        userInput = event.key;
-        console.log(userInput);
-
-        //Letter returns T/F
-        if (userInput === randLetter){
-            alert ("You got it!");
-
+function userInput (){
+    var userInput = document.getElementById("guess");
+        userInput.onkeyup = function(userGuess){
+            for(i=0;i<letters.length;i++){
+                if(userInput == randLetter){
+                    var wins = document.getElementById("win");
+                    for(j=0;j=wins;j++){
+                        var winSet = [j] + 1;
+                        var addWin = document.createElement("wins")
+                        addWin.innerHTML = winSet;
+                        wins.appendChild(addWin);
+                    }
+                    alert("You guessed correctly!");
+                }
+                else{
+                    
+                }
+            }
         }
-    }
-
-
-    //T = Win+1, Reset
-
-    //F = Display letter, guess left -1, Guess again
-
-        //@10 guesses = Loss +1, Reset cpu letter 
-
-        
+}
 
 
